3117e6bce480432d56cb9945730aa9eb110f3428
saravanan <sarancruzer@gmail.com>
2017-11-27 15:51:43 +0530

    cors issue changes

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,3 +1,10 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+
+?>
 <!doctype html>
 <html ng-app="listeoApp">
     <head>
@@ -100,3 +107,4 @@
         
 
 </html>
+
