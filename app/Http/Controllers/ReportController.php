8d248ad26353a164cd8871cfeaedcc0d4629c8bd
Saravanan Nandhan <sarancruzer@gmail.com>
2018-02-28 14:47:12 +0530

    changes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -101,6 +101,117 @@ class ReportController extends Controller
         //
     }
 
+    public function getClientReportList(Request $request)
+    {
+        //
+        $token = $this->getToken($request);
+        $user = JWTAuth::toUser($token);
+        $input = $request->all();
+        
+        //echo public_path();
+        //return PDF::loadHTML($str)->save(public_path().'/my_stored_file1.pdf')->download('download.pdf');
+
+        // $pdf = PDF::loadView('pdf.invoice', $data);
+        // return $pdf->download('invoice.pdf');
+
+        // $pdf = App::make('dompdf.wrapper');
+        // $pdf->loadHTML('<h1>Test</h1>');
+        // return $pdf->download("file.pdf");
+
+        
+
+        $lists = DB::table('clients as c')
+                    ->leftjoin('m_client_type as mct','mct.id','=','c.business_type')
+                    ->leftjoin('m_client_size as mcs','mcs.id','=','c.client_size')
+                    ->leftjoin('license as l','l.id','=','c.manage_license')
+                    ->leftjoin('m_state as ms','ms.id','=','c.state')
+                    ->leftjoin('m_contacts as mc','mc.client_name','=','c.id')                   
+                    
+                    ->select('c.*','mct.name as client_type','mcs.name as client_size','l.license_trading_name as licensee','ms.name as state',DB::raw("COUNT(mc.id) AS contacts_no"))
+                    ->groupby('c.id')
+                    ->get();
+
+        $str= '    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                    <table width="100%" cellspacing="2" cellpadding="2" border="1" >
+                    <thead>
+                    <style>
+                    thead {backgroun:darkgrey;}
+                    table, th, td {
+                        border: 1px solid black;
+                    }
+                    </style>
+                    <tr >
+                    <th colspan="10" align="center"><h3>Client Activity Report</h3></th>
+                    </tr>
+
+                        <tr >
+                            <th>Client Name</th>
+                            <th>Client Type</th>
+                            <th>Client Size</th>
+                            <th>Licensee</th>
+                            <th>State</th>
+                            <th>No.of Contacts</th>
+                            <th>Total Projects</th> 
+                            <th>Total Quotes (no)</th> 
+                            <th>Total Quotes($)</th> 
+                            <th>Total Sales($)</th> 
+                        </tr>
+                    </thead>
+                    <tbody>';
+        
+                    foreach ($lists as $key => $value) {
+
+                        $projects = DB::table('project as p')
+                                    ->select('p.*')
+                                    ->where('p.client_name','=',$value->id)
+                                    ->get();
+
+                                    $quote_count = 0;;
+                                    $quote_value = 0;       
+
+                                    foreach ($projects as $k => $val) {
+                                       
+
+                                        $pquote = DB::table('project_scope_quote as psc')
+                                            ->select('psc.*')
+                                            ->where('psc.project_id','=',$val->id)
+                                            ->first();
+
+                                        if(count($pquote)>0){
+                                                                                   
+                                            $quote_count++;
+                                            $quote_value = $quote_value + $pquote->quote;        
+                                        }   
+                                        
+                                    }                       
+                        
+                        $str .= '<tr>
+                                    <td>'.$value->client_business.'</td>
+                                    <td>'.$value->client_type.'</td>
+                                    <td>'.$value->client_size.'</td>
+                                    <td>'.$value->licensee.'</td>
+                                    <td>'.$value->state.'</td>
+                                    <td>'.$value->contacts_no.'</td>
+                                    <td>'.count($projects).'</td>
+                                    <td>'.$quote_count.'</td>
+                                    <td>'.$quote_value.'</td>
+                                    <td>'.$quote_value.'</td>
+                                </tr>';
+                    }
+
+                    $str .= '</tbody></table></div></div></div>';
+
+
+         $pdf = App::make('dompdf.wrapper');
+         $pdf->loadHTML($str);
+         $pdf->setPaper('A4', 'landscape');
+         return $pdf->download("file.pdf");
+    
+
+        
+    }
+
+
     public function getClientReport(Request $request)
     {
         //
@@ -210,6 +321,8 @@ class ReportController extends Controller
 
         
     }
+
+    
 
     public function getProjectReport(Request $request)
     {
