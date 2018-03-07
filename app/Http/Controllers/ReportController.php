3f5a40405346f03b2fcccc988e8ba53384812077
Saravanan Nandhan <sarancruzer@gmail.com>
2018-03-07 15:03:26 +0530

    project details report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -101,116 +101,6 @@ class ReportController extends Controller
         //
     }
 
-    public function getClientReportList(Request $request)
-    {
-        //
-        $token = $this->getToken($request);
-        $user = JWTAuth::toUser($token);
-        $input = $request->all();
-        
-        //echo public_path();
-        //return PDF::loadHTML($str)->save(public_path().'/my_stored_file1.pdf')->download('download.pdf');
-
-        // $pdf = PDF::loadView('pdf.invoice', $data);
-        // return $pdf->download('invoice.pdf');
-
-        // $pdf = App::make('dompdf.wrapper');
-        // $pdf->loadHTML('<h1>Test</h1>');
-        // return $pdf->download("file.pdf");
-
-        
-
-        $lists = DB::table('clients as c')
-                    ->leftjoin('m_client_type as mct','mct.id','=','c.business_type')
-                    ->leftjoin('m_client_size as mcs','mcs.id','=','c.client_size')
-                    ->leftjoin('license as l','l.id','=','c.manage_license')
-                    ->leftjoin('m_state as ms','ms.id','=','c.state')
-                    ->leftjoin('m_contacts as mc','mc.client_name','=','c.id')                   
-                    
-                    ->select('c.*','mct.name as client_type','mcs.name as client_size','l.license_trading_name as licensee','ms.name as state',DB::raw("COUNT(mc.id) AS contacts_no"))
-                    ->groupby('c.id')
-                    ->get();
-
-        $str= '    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-                    <table width="100%" cellspacing="2" cellpadding="2" border="1" >
-                    <thead>
-                    <style>
-                    thead {backgroun:darkgrey;}
-                    table, th, td {
-                        border: 1px solid black;
-                    }
-                    </style>
-                    <tr >
-                    <th colspan="10" align="center"><h3>Client Activity Report</h3></th>
-                    </tr>
-
-                        <tr >
-                            <th>Client Name</th>
-                            <th>Client Type</th>
-                            <th>Client Size</th>
-                            <th>Licensee</th>
-                            <th>State</th>
-                            <th>No.of Contacts</th>
-                            <th>Total Projects</th> 
-                            <th>Total Quotes (no)</th> 
-                            <th>Total Quotes($)</th> 
-                            <th>Total Sales($)</th> 
-                        </tr>
-                    </thead>
-                    <tbody>';
-        
-                    foreach ($lists as $key => $value) {
-
-                        $projects = DB::table('project as p')
-                                    ->select('p.*')
-                                    ->where('p.client_name','=',$value->id)
-                                    ->get();
-
-                                    $quote_count = 0;;
-                                    $quote_value = 0;       
-
-                                    foreach ($projects as $k => $val) {
-                                       
-
-                                        $pquote = DB::table('project_scope_quote as psc')
-                                            ->select('psc.*')
-                                            ->where('psc.project_id','=',$val->id)
-                                            ->first();
-
-                                        if(count($pquote)>0){
-                                                                                   
-                                            $quote_count++;
-                                            $quote_value = $quote_value + $pquote->quote;        
-                                        }   
-                                        
-                                    }                       
-                        
-                        $str .= '<tr>
-                                    <td>'.$value->client_business.'</td>
-                                    <td>'.$value->client_type.'</td>
-                                    <td>'.$value->client_size.'</td>
-                                    <td>'.$value->licensee.'</td>
-                                    <td>'.$value->state.'</td>
-                                    <td>'.$value->contacts_no.'</td>
-                                    <td>'.count($projects).'</td>
-                                    <td>'.$quote_count.'</td>
-                                    <td>'.$quote_value.'</td>
-                                    <td>'.$quote_value.'</td>
-                                </tr>';
-                    }
-
-                    $str .= '</tbody></table></div></div></div>';
-
-
-         $pdf = App::make('dompdf.wrapper');
-         $pdf->loadHTML($str);
-         $pdf->setPaper('A4', 'landscape');
-         return $pdf->download("file.pdf");
-    
-
-        
-    }
-
 
     public function getClientReport(Request $request)
     {
@@ -477,6 +367,87 @@ class ReportController extends Controller
          $str .= '</tbody></table></div></div></div>';
 
         
+        //  $items_list = DB::table('m_items')->get();
+
+        //  $project_scope_list = DB::table('project_scope_new as ps')
+        //                         ->leftjoin('m_project_type as pt','pt.id','=','ps.project_type_id')
+        //                         ->leftjoin('m_building_class as bc','bc.id','=','ps.building_class_id')
+        //                         ->where('ps.project_id','=',$id)
+        //                         ->select('ps.*','pt.name as project_type_name','bc.name as building_class_name')
+        //                         ->get();
+         
+        //  $str .= '<br><br><br>';
+
+        // $str .= '    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        //  <table class="table">
+        //       <thead class="thead-inverse">
+        //         <tr>
+        //           <th>Items</th>';
+        //           foreach ($project_scope_list as $key => $value) {
+        //             $str .= '<th >'. $value->building_class_name . '</th>';
+        //           }                  
+        // $str .= '</tr></thead><tbody>';
+
+
+
+
+
+        // $ps_lists = DB::table('project_scope_new')->where('project_id','=',$id)->get();
+
+        // $arr=[];
+        // $arrr = [];
+
+        // foreach ($ps_lists as $key => $value) {
+
+        //     $ps_child_items = DB::table('project_scope_child_new')->where('ps_id','=',$value->id)->get();
+
+        //     foreach ($ps_child_items as $k => $val) {
+
+        //         $item = DB::table('m_items')->where('id','=',$val->items_id)->first();
+
+        //         $project_types = DB::table('m_project_type')->where('id','=',$value->project_type_id)->first();
+        //         $building_classs = DB::table('m_building_class')->where('id','=',$value->building_class_id)->first();
+
+        //         $arr[$key]['project_id'] = $value->project_id;
+        //         $arr[$key]['project_type_id'] = $value->project_type_id;
+        //         $arr[$key]['building_class_id'] = $value->building_class_id;
+                
+        //         $arr[$key]['project_type'] = $project_types->name;
+        //         $arr[$key]['building_class'] = $building_classs->name;
+        //         $arr[$key]['building_units'] = $value->building_units;
+
+        //         $arr[$key][$item->db_name] = $val->qty;
+                
+        //         if($val->items_id != 18){
+
+        //         $priceLists = DB::table('m_project_scope_new')
+        //                     ->where('building_class_id','=',$value->building_class_id)
+        //                     ->where('items_id','=',$val->items_id)
+        //                     ->first();
+
+        //         $arr[$key][$item->db_name.'_price'] = $priceLists->price;
+
+
+        //         $arrr[$key]['project_type'] = $project_types->name;
+        //         $arrr[$key]['building_class'] = $building_classs->name;
+        //         $arrr[$key]['building_units'] = $value->building_units;
+
+        //         $arrr[$key][$item->db_name] = $val->qty;
+
+        //         }
+
+
+
+               
+                
+        //     }  
+
+        // }
+
+
+        // $result['info']['lists'] = $arr;
+        // $result['info']['items_lists'] = $arrr;
+       
 
 
          $pdf = App::make('dompdf.wrapper');
